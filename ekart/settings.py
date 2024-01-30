5f17815d3b35f44a96846d8587a59ccc6a9c9098
Tom Sebastian <94066515+ptomsebastian@users.noreply.github.com>
2024-01-30 21:29:53 +0530

    Update settings.py

--- ekart/settings.py
+++ ekart/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_as*nfvd7g-x2z6a^fspv+fgk6+gsa*zeeo7p1n!gn8znka!se'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
